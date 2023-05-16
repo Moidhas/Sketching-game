52c2340f398893b2cd8061d634d064890e90e7c9
Moidhas <moidk3026@gmail.com>
2023-05-16 18:12:56 -0400

    Finished grid feature

---
+++ java.js
@@ -0,0 +1,9 @@
+
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    container.appendChild(div);
+}
+
