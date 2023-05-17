142eee7d0aed0809738df9deb6e8098b99722f51
Moidhas <moidk3026@gmail.com>
2023-05-17 12:44:54 -0400

    Added resizing of grid through sliders

--- java.js
+++ java.js
@@ -9,24 +9,38 @@ function fillIn(event) {
 
 function end(event) {
     divs.forEach(div => div.removeEventListener('mouseover', fillIn));
-    console.log(this);
 }
 
+function setup(event) {
+    text.textContent = `${input.value} x ${input.value}`;
+    let length = input.value * input.value; 
+    let size = (1 / input.value) * 100; 
+    for (let i = 0; i < length; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = `${size}%`;
+        div.style.height = `${size}%`;
+        container.appendChild(div);
+    } 
+}
 
+const controls = document.querySelector('.controls');
+const input = document.querySelector('input');
+const text = document.createElement('div');
+text.classList.add('values');
 const container = document.querySelector('.container');
-const body = document.querySelector('body');
-let length = 15 * 15; 
-let size = (1 / 15) * 100; 
+const html = document.querySelector('html');
 
-for(let i = 0; i < length; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    div.style.width = `${size}%`;
-    div.style.height = `${size}%`;
-    container.appendChild(div);
-}
+setup();
+let divs = document.querySelectorAll('.grid');
+controls.insertBefore(text, input);
 
-const divs = document.querySelectorAll('.grid');
 divs.forEach(div => div.addEventListener('mousedown', start));
 divs.forEach(div => div.addEventListener('mouseup', end));
-body.addEventListener('mouseup', end);
+html.addEventListener('mouseup', end);
+
+input.addEventListener('input', () => {
+    divs.forEach(div => container.removeChild(div));
+    setup();
+    divs = document.querySelectorAll('.grid'); 
+});
