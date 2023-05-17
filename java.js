96e8a50e2a164e933520c8a6f065fcd705dcbea4
Moidhas <moidk3026@gmail.com>
2023-05-16 23:23:35 -0400

    Moved grid algorithm over to JavaScript

--- java.js
+++ java.js
@@ -1,13 +1,3 @@
-
-const container = document.querySelector('.container');
-let length = 16 * 16; 
-for(let i = 0; i < length; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
-}
-
-
 function start(event) {
     this.classList.add('clicked');
     divs.forEach(div => div.addEventListener('mouseover', fillIn));
@@ -19,6 +9,19 @@ function fillIn(event) {
 
 function end(event) {
     divs.forEach(div => div.removeEventListener('mouseover', fillIn));
+}
+
+
+const container = document.querySelector('.container');
+let length = 16 * 16; 
+let size = (1 / 16) * 100; 
+
+for(let i = 0; i < length; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    div.style.width = `${size}%`;
+    div.style.height = `${size}%`;
+    container.appendChild(div);
 }
 
 const divs = document.querySelectorAll('.grid');
